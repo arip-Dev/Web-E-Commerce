a28728bdd9b994b63d4510b6f0200a78597b3432
Arif <kudaayey@gmail.com>
2025-07-10 09:52:02 +0700

    perbaikan code untuk deploy

--- src/components/HomePage/MainPage6.jsx
+++ src/components/HomePage/MainPage6.jsx
@@ -1,7 +1,10 @@
+import Group3 from '../../assets/Group3.png';
+
 function MainPage6() {
     return (
         <div className="flex bg-white justify-center items-center">
-        <div className="bg-[url(./src/assets/Group3.png)] text-black bg-cover bg-center flex flex-col items-center justify-center w-full h-[450px]">
+        <div className="text-black bg-cover bg-center flex flex-col items-center justify-center w-full h-[450px]"
+        style={{ backgroundImage: `url(${Group3})` }}>
             <h1 className="text-5xl font-bold mb-3">Our Instagram</h1>
             <p className="mb-3">Follow our store on Instagram</p>
             <button className="btn btn-wide rounded-4xl bg-white text-black border-none"
